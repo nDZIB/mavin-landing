fc1d9605383a2bfe1473f8554fd592aaa7958355
ndzib <serkwi.bruno@go-groups.net>
2023-08-17 11:45:30 +0100

    completed hero section less animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentStep = 4;
+  steps:number[] = [1, 2,3, 4];
+  faceArtists: string[] = [
+    'assets/artist_x.png',
+    'assets/artist_y.png',
+    'assets/artist_z.png',
+    'assets/artist_x.png',
+  ]
+  navs = [
+    {
+      label: 'Home',
+      route: '/',
+      exact: true
+    },
+    {
+      label: 'About',
+      route: '#about',
+      exact: true
+    },
+    {
+      label: 'Services',
+      route: '#services',
+      exact: true
+    },
+    {
+      label: 'Gallery',
+      route: '#gallery',
+      exact: true
+    },
+    {
+      label: 'Artists',
+      route: '#artists',
+      exact: true
+    },
+    {
+      label: 'Blog',
+      route: '#blog',
+      exact: true
+    },
+    {
+      label: 'Career',
+      route: '#career',
+      exact: true
+    }
+  ]
+
   artists: any = [
     {
       name: 'Keneth Tim',
